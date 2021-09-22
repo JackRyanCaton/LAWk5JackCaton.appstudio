f11b924febf5eef84538e61f6d8e01627ee1f410
JackRyanCaton <jrc88858@creighton.edu>
2021-09-22 18:15:53 -0500

    cleaning up main

--- forms/Calc/Calc.js
+++ forms/Calc/Calc.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 /*function Calculate(num1, num2) {
+=======
+function Calculate(num1, num2) {
+>>>>>>> Stashed changes
   num1 += num2
   return (num1); }
   
@@ -6,5 +10,9 @@ let n1 = parseInt(prompt("Insert a number"))
 let n2 = parseInt(prompt("Insert a number"))
 
 let summedNumbers = Calculate(n1, n2)
+<<<<<<< Updated upstream
 console.log(`The sum of ${n1} and ${n2} is ${summedNumbers}`)
 */
+=======
+console.log(`The sum of ${n1} and ${n2} is ${summedNumbers}`)
+>>>>>>> Stashed changes
--- forms/While/While.js
+++ forms/While/While.js
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 /*
+=======
+>>>>>>> Stashed changes
 let stop = prompt("How many times should this loop iterate?")
 i = 0
 while (i < stop) {
